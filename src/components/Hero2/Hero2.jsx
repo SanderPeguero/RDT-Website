d1c2d20a69e322cbebe9db97dcb6a68b935428f1
jose22gonzalez <urenajosealbeto@gmail.com>
2023-11-29 22:12:13 -0400

    RW-3-Hero-2

--- src/components/Hero2/Hero2.jsx
+++ src/components/Hero2/Hero2.jsx
@@ -1,7 +1,4 @@
-import BaseBottom from "./Base Bottom.png"
-import Base from "./Base.png"
-import grip from "./grip.png"
-import sCREENMASK from "./SCREEN MASK.png"
+import container from "./container.png"
 
 //RW-3
 function Hero2() {
@@ -25,37 +22,12 @@ function Hero2() {
                     Try For Free
                 </button>
             </div>
+           
 
-            <div className="mt-[4rem]">
+            <div className="mt-[4rem] ">
 
-                <div className="flex items-center justify-center">
+                <img src={container} />
 
-
-                    <div
-                        className="flex items-center justify-center border-[8.93px] border-solid border-gray-300 rounded-lg bg-black w-[421.57px] md:w-[843.14px] h-[283.63px] md:h-[567.26px] left-[46.64] md:left-[93.28px]"
-                        style={{ borderRadius: "26.79px, 26.79px, 0px, 0px" }}
-                    >
-                        <img
-                            className=" w-[398px] md:w-[796px] h-[248.905px] md:h-[497.81px] top-[11.64px] md:top-[23.28px] left-[58.28px]  md:left-[116.56px]"
-                            src={sCREENMASK}
-
-                        />
-                    </div>
-                </div>
-
-                <div className="flex items-center justify-center">
-                    <img src={grip} />
-                </div>
-                <div className="">
-
-
-                    <img 
-                    className="w-[1033.26px] h-[21.34px] top-[544.37px] "
-                    src={Base} />
-                </div>
-                <div className="">
-                    <img src={BaseBottom} />
-                </div>
 
             </div>
         </div>
