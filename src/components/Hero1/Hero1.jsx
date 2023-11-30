e49412cc3cd171b2c05104ac7454f558efc35078
jose22gonzalez <urenajosealbeto@gmail.com>
2023-11-30 12:28:48 -0400

    responsive ROW-11-Hero-1

--- src/components/Hero1/Hero1.jsx
+++ src/components/Hero1/Hero1.jsx
@@ -3,21 +3,27 @@ import background2 from "./background.jpg";
 function Hero1() {
     return (
         <div className="relative">
-            <img src={background2} className="w-full" alt="Background" />
+            <img src={background2} className="w-full h-[50rem]" alt="Background" />
             <div className="absolute inset-0 flex items-center justify-center">
-                <div className="flex flex-row items-center justify-center">
+                <div className="flex flex-col-reverse md:flex-row items-center justify-center">
+                <div className="mt-[7rem] block md:hidden">
+                        <button className="w-[236px] h-[65px] p-[16px, 0px] rounded-full bg-blue-500 text-white font-roboto font-semibold text-lg leading-[33px] tracking-[0.1px] text-center">
+                            Try For Free
+                        </button>
+                    </div>
                     <img
                         src={TicketSystem}
-                        className="m-[2rem]"
+                        className="m-[2rem] md:m-[2rem]"
                     />
-                    <div className="flex flex-col">
+                  
+                    <div className="flex flex-col mt-[4rem]">
                         <h1 className="font-roboto font-extrabold text-5xl leading-66 tracking-[0.2px] text-white w-[321px] h-[131px]">
                             We focus on ergonomics
                         </h1>
                         <h4 className="font-roboto font-bold text-base leading-20 tracking-[0.2px] text-white w-[298px] h-[40px]">
                             Most calendars are designed for teams. Slate is designed for freelancers
                         </h4>
-                        <div className="mt-[4rem]">
+                        <div className="mt-[4rem] hidden md:block">
                             <button className="w-[236px] h-[65px] p-[16px, 0px] rounded-full bg-blue-500 text-white font-roboto font-semibold text-lg leading-[33px] tracking-[0.1px] text-center">
                                 Try For Free
                             </button>
