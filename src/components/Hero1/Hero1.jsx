c9b043693011a6b72041a2f3a7d346d11d708041
jose22gonzalez <urenajosealbeto@gmail.com>
2023-12-01 22:09:38 -0400

    Adjusted hero size and text

--- src/components/Hero1/Hero1.jsx
+++ src/components/Hero1/Hero1.jsx
@@ -1,36 +1,32 @@
 import TicketSystem from "./Ticket System Mockup.png";
 import background2 from "./background.jpg";
+//RW-11
 function Hero1() {
     return (
         <div className="relative">
-            <img src={background2} className="w-full h-[50rem]" alt="Background" />
+            <img src={background2} className="w-full  h-[45rem] " alt="Background" />
+
             <div className="absolute inset-0 flex items-center justify-center">
                 <div className="flex flex-col-reverse md:flex-row items-center justify-center">
-                <div className="mt-[7rem] block md:hidden">
-                        <button className="w-[236px] h-[65px] p-[16px, 0px] rounded-full bg-blue-500 text-white font-roboto font-semibold text-lg leading-[33px] tracking-[0.1px] text-center">
+                    <div className="mt-[4rem] block md:hidden">
+                        <button className="w-[200px] h-[50px] p-[12px, 0px] rounded-full bg-blue-500 text-white font-roboto font-semibold text-base leading-[28px] tracking-[0.1px] text-center">
                             Try For Free
                         </button>
                     </div>
-                    <img
-                        src={TicketSystem}
-                        className="m-[2rem] md:m-[2rem]"
-                    />
-                  
-                    <div className="flex flex-col mt-[4rem]">
-                        <h1 className="font-roboto font-extrabold text-5xl leading-66 tracking-[0.2px] text-white w-[321px] h-[131px]">
+                    <img src={TicketSystem} className="m-[1rem] md:m-[1rem]" />
+                    <div className="flex flex-col mt-[1rem]">
+                        <h1 className="font-roboto font-extrabold text-3xl leading-50 tracking-[0.2px] text-white w-[260px] h-[100px]">
                             We focus on ergonomics
                         </h1>
-                        <h4 className="font-roboto font-bold text-base leading-20 tracking-[0.2px] text-white w-[298px] h-[40px]">
+                        <h4 className="font-roboto font-bold text-sm leading-16 tracking-[0.2px] text-white w-[240px] h-[30px]">
                             Most calendars are designed for teams. Slate is designed for freelancers
                         </h4>
-                        <div className="mt-[4rem] hidden md:block">
-                            <button className="w-[236px] h-[65px] p-[16px, 0px] rounded-full bg-blue-500 text-white font-roboto font-semibold text-lg leading-[33px] tracking-[0.1px] text-center">
+                        <div className="mt-[3rem] hidden md:block">
+                            <button className="w-[200px] h-[50px] p-[12px, 0px] rounded-full bg-blue-500 text-white font-roboto font-semibold text-base leading-[28px] tracking-[0.1px] text-center">
                                 Try For Free
                             </button>
                         </div>
                     </div>
-
-
                 </div>
             </div>
         </div>
