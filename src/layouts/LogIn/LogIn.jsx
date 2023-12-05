4568ef9a9c24a0aec77b6d61aa454a28eb1aee7e
Daniel8529 <carlosdanielmonterogomez@gmail.com>
2023-12-05 18:41:02 -0400

    RW-23-LogIn

--- src/layouts/LogIn/LogIn.jsx
+++ src/layouts/LogIn/LogIn.jsx
@@ -63,7 +63,7 @@ const LogIn = () => {
                             />
 
                             <Link
-                            to={"/"}
+                            to={"/admin"}
                                 type="submit"
                                 className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
                             >
