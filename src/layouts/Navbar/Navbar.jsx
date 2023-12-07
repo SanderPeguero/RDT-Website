8bf6fd3a1420248c560749171b10167e7815ff74
jose22gonzalez <urenajosealbeto@gmail.com>
2023-12-07 12:15:27 -0400

    Fixing only NavBar

--- src/layouts/Navbar/Navbar.jsx
+++ src/layouts/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdOutlineSort } from "react-icons/md";
-import logo from "./Logo.png"
 import { Link } from 'react-router-dom';
-import { navLinks } from "./constants/route"
-function Navbar({ setisAdmin, isAdmin }) {
+import logo from "./Logo.png"
+import { navLinks } from './constants/route';
+function Navbar() {
 
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
@@ -11,20 +11,15 @@ function Navbar({ setisAdmin, isAdmin }) {
     setMobileMenuOpen(!isMobileMenuOpen);
   };
 
-  const ChangeTypeUser = (e) => {
-    e.preventDefault();
-    console.log("Actual: " + isAdmin);
+  const logout = () => {
+    localStorage.clear()
+    window.location.href = ''
+  }
 
-    setisAdmin(prevIsAdmin => {
-      if (prevIsAdmin === true) {
-        console.log("User: " + prevIsAdmin);
-        return false;
-      } else {
-        console.log("Admin: " + prevIsAdmin);
-        return true;
-      }
-    });
-  };
+  const authAdmin = localStorage.getItem('D')
+  useEffect(() => {
+    console.log(authAdmin)
+  }, [authAdmin])
 
   // <nav className=" max-w-full px-2 sm:px-6 md:px-24 bg-[#000320]">
   // <div className="relative flex h-16 items-center justify-between">
@@ -36,38 +31,32 @@ function Navbar({ setisAdmin, isAdmin }) {
           <img src={logo} alt="Logo" className="h-12 mr-2" />
 
         </div>
-        {/* Botones para pantallas grandes */}
         {
-          isAdmin ? <div className="hidden md:flex space-x-8">
-            <button className="text-white">Home</button>
-            <button className="text-white">Product</button>
-            <button className="text-white">Pricing</button>
-            <button className="text-white">About</button>
-            <button className="text-white">Contact</button>
-            <button onClick={ChangeTypeUser} className='text-white'>
-              {isAdmin ? <span>Change admin</span> : <span>Change user</span>}
-            </button>
-          </div> :
-            <>
-              <ul className="hidden md:flex space-x-8">
-                {
-                  navLinks.map((nav, index) => (
-                    <li key={index} className={``}>
-                      <a href={`#${nav.id}`} className='text-white px-1 py-2 mt-2 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline'>{nav.title}</a>
-                    </li>
-                  ))
-                }
-                <button onClick={ChangeTypeUser} className='text-white'>
-                  {isAdmin ? <span>Change admin</span> : <span>Change user</span>}
-                </button>
-              </ul>
+          authAdmin === null ? <>
+            <ul className="hidden md:flex space-x-8">
+              {
+                navLinks.map((nav, index) => (
+                  <li key={index} className={``}>
+                    <a href={`#${nav.id}`} className='text-white px-1 py-2 mt-2 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline'>{nav.title}</a>
+                  </li>
+                ))
+              }
 
-            </>
+            </ul>
+
+          </> :
+
+            <div className="hidden md:flex space-x-8">
+              {/* Botones para pantallas grandes */}
+              <button className="text-white">Home</button>
+              <button className="text-white">Product</button>
+              <button className="text-white">Pricing</button>
+              <button className="text-white">About</button>
+              <button className="text-white">Contact</button>
+              <button onClick={logout} className="text-white">Logout</button>
+            </div>
 
         }
-
-
-
 
 
         {/* Menú desplegable para pantallas móviles */}
@@ -79,20 +68,33 @@ function Navbar({ setisAdmin, isAdmin }) {
             <MdOutlineSort size={30} style={{ transform: 'scaleX(-1)' }} />
           </button>
           {isMobileMenuOpen && (
-            <ul className="absolute top-16 left-0 right-0 bg-[#000320] py-2 flex flex-col items-center  z-50">
+            <>
               {
-                navLinks.map((nav, index) => (
-                  <li key={index} className={``}>
-                    <a href={`#${nav.id}`} className='block text-white px-4 py-2'>{nav.title}</a>
-                  </li>
-                ))
+                authAdmin === null ?
+                  <>
+                    <ul className="absolute top-16 left-0 right-0 bg-[#000320] py-2 flex flex-col items-center  z-50">
+                      {
+                        navLinks.map((nav, index) => (
+                          <li key={index} className={``}>
+                            <a href={`#${nav.id}`} className='block text-white px-4 py-2'>{nav.title}</a>
+                          </li>
+                        ))
+                      }
+                    </ul>
+                  </>
+                  : <>
+                    <div className="absolute top-16 left-0 right-0 bg-[#000320] py-2 flex flex-col items-center  z-50">
+                      <button className="block text-white px-4 py-2">Home</button>
+                      <button className="block text-white px-4 py-2">Product</button>
+                      <button className="block text-white px-4 py-2">Pricing</button>
+                      <button className="block text-white px-4 py-2">About</button>
+                      <button className="block text-white px-4 py-2">Contact</button>
+                      <button onClick={logout} className="text-white">Logout</button>
+                    </div>
+                  </>
               }
-              {/* <button className="block text-white px-4 py-2">Home</button>
-              <button className="block text-white px-4 py-2">Product</button>
-              <button className="block text-white px-4 py-2">Pricing</button>
-              <button className="block text-white px-4 py-2">About</button>
-              <button className="block text-white px-4 py-2">Contact</button> */}
-            </ul>
+            </>
+
           )}
         </div>
       </div>
